3bfea061c943fe29810e71d94d3d9bf5617d2849
Pultarova <114436774+Pultarova@users.noreply.github.com>
2023-11-09 20:08:03 +0100

    Add files via upload

---
+++ cviceni6.m
@@ -0,0 +1,67 @@
+% MKP pro -(au')'+pu=f, u(0)=OP1, u(L)=OP2
+function abc
+global elem
+global uzly
+L = 2; % interval (0,L)
+OP1 = -1; 
+OP2 = 0;
+N = 10; % pocet elementu
+uzly = linspace(0,L,N+1); % seznam uzlovych hodnot
+elem = zeros(N,2); 
+for k = 1:N % seznam uzlu na jednotlivych elementech
+    elem(k,:) = [k,k+1];
+end;
+A = zeros(N+1); % matice soustavy
+B = zeros(N+1,1); % prava strana soustavy
+for k = 1:N
+    % cisla bazovych funkci (= cisla uzlu) na tomto elementu:
+    kde = elem(k,:); 
+    % pricteni integralu z tohoto elementu na nektere pozice matice A:
+    A(kde,kde) = A(kde,kde) + Integruj_a(k) + Integruj_p(k); 
+    % pricteni integralu z tohoto elementu na nektere pozice vektoru B:
+    B(kde) = B(kde) + Integruj_f(k);
+end;
+B = B - A(:,1)*OP1; % uplatneni leve OP
+B = B - A(:,N+1)*OP2; % uplatneni prave OP
+A = A(2:N,2:N); % vlastni matice soustavy 
+B = B(2:N); % vlastni rava strana
+U = A\B; % reseni
+cla; hold on;
+plot(uzly,[OP1;U;OP2]); % kresleni reseni
+plot(uzly,uzly*0,'k.'); % kresleni uzlu
+% --------------------------------------------------
+function z = Integruj_a(k) % numericka integrace integralu s "a"
+global elem
+global uzly
+un = elem(k,:);
+x = uzly(un);
+h = x(2)-x(1);
+xs = (x(1)+x(2))/2;
+z = Fcea(xs)*[1,-1;-1,1]/h;
+
+function z = Integruj_p(k) % numericka integrace integralu s "p"
+global elem
+global uzly
+un = elem(k,:);
+x = uzly(un);
+h = x(2)-x(1);
+xs = (x(1)+x(2))/2;
+z = Fcep(xs)*[1/3,1/6;1/6,1/3]*h;
+
+function z = Integruj_f(k)  % numericka integrace integralu s "f"
+global elem
+global uzly
+un = elem(k,:);
+x = uzly(un);
+h = x(2)-x(1);
+xs = (x(1)+x(2))/2;
+z = Fcef(xs)*[1;1]*h/2;
+
+function z = Fcea(x)
+z = (x-0.5)^2+0.001;
+function z = Fcep(x)
+z = 0;
+function z = Fcef(x)
+z = 1;
+
+
